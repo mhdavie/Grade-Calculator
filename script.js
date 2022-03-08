6022a6af7301752141e119d2457862025a6beb5b
mdavie <marianahodniki1991@gmail.com>
2022-03-07 22:52:17 -0700

    first challenge finished

---
+++ script.js
@@ -0,0 +1,20 @@
+var studentName =  "Mariana"
+var firstBimester = 9.58
+var secondBimester = 7.59
+var thirdBimester = 4.37
+var fourthBimester = 2.35
+
+var finalGrade = (firstBimester + secondBimester + thirdBimester + fourthBimester) / 4
+
+// var fixedGrade = finalGrade.toFixed()
+
+// var halfYear = (firstBimester + secondBimester)
+
+// var secondHalfYear = (thirdBimester + fourthBimester)
+
+// var finalGrade = (halfYear + secondHalfYear) / 4
+
+
+console.log (finalGrade)
+
+
